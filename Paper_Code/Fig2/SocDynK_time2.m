8b6800856d8c646eca8c2858ed1739c83dda5ff5
David Bonini <21473447@student.curtin.edu.au>
2025-07-15 19:53:01 +0800

    Style changes for fig2a

--- Paper_Code/Fig2/SocDynK_time2.m
+++ Paper_Code/Fig2/SocDynK_time2.m
@@ -1,8 +1,7 @@
-function [t,dt,y] = SocDynK_time2(n,beta,r,k,s) 
+function [t,dt,y] = SocDynK_time2(n,beta,r,k,s,rho) 
 %s is the number of committed minority
 % k and r are arrays of n*1 where the first n-s entries are k_e, followed
 % by k_f
-y=zeros(n,1); %Creates an array of 0s (n rows, 1 column)
 n_s=sum(s)/n; %Defines the percentage of committed minority
 if length(s)==1
     s=[zeros(n-s,1); ones(s,1)]; %Creates an nx1 array where the first (n-s) rows are 0, following s are 1
@@ -29,61 +28,78 @@ if size(r,1)==1
 end
 b=ones(n,1)-k-r; %b is the remainder of weights from k and r
 %From definition of b+k+r=1
-
-old=zeros(n,1)+s; %Creates an nx1 array all of value s (Remembering the s parameter has been modified in code)
-x=old;
-p=.5; %Sets starting value for x_hat
-t=0; %Sets starting value for t
-flag=0;
-dt=0;
-z=n*n_s; %=Number of committed minority?
 g=1;
-while flag==0 && t<10000
-    t=t+1;
-    Total_equals=[];
-    G=ones(n,n);
-    for v=1:n
-        equals=-1;
-        for w=1:n
-            if x(v)==x(w)
-                equals=equals+1;
-                G(v,w)=g;
+for runs=1:10
+    y=zeros(n,1); %Creates an array of 0s (n rows, 1 column)
+    old=zeros(n,1)+s; %Creates an nx1 array all of value s (Remembering the s parameter has been modified in code)
+    x=old;
+    p=.5; %Sets starting value for x_hat
+    t=0; %Sets starting value for t
+    flag=0;
+    dt=0;
+    z=n*n_s; %=Number of committed minority
+    while flag==0 && t<600
+        t=t+1;
+        Total_equals=[];
+        G=ones(n,n);
+        for v=1:n
+            equals=-1;
+            for w=1:n
+                if x(v)==x(w)
+                    equals=equals+1;
+                    G(v,w)=g;
+                end
+                if w==v
+                    G(v,w)=0;
+                end
             end
-            if w==v
-                G(v,w)=0;
-            end
+            Total_equals=[Total_equals; equals];
         end
-        Total_equals=[Total_equals; equals];
+        Total_unequals=n-1-Total_equals;
+        a_base = b./(g*Total_equals + Total_unequals);
+        denominator=(g*Total_equals + Total_unequals);
+        a_all=a_base.*G;
+        P_1=x'.*a_all;
+        S_C_1=sum(P_1,2);
+        P_0=(1-x').*a_all;
+        S_C_0=sum(P_0,2);
+        pi(:,1)=S_C_0+k.*(1-x)+r.*(1-p); %SQ (0)
+        pi(:,2)=S_C_1+k.*x+r.*p; %Alt (1)
+        x=zeros(n,1);
+        x(rand(n,1)<exp(beta*pi(:,2))./(exp(beta*pi(:,2))+exp(beta*pi(:,1))))=1;
+        %If rand number [0,1] < prob(alt), then agent plays alt
+        x(s==1)=1; %Those who are CM will play strat 1 (alt)
+        p=.5*(1+(sum(x)-x)/(n-1)-(sum(old)-old)/(n-1)); %Updates x_hat
+        z=[z sum(x)];
+        if sum(x)<=.4*n
+            dt=t; %dt will stop at take-off time
+        end
+        % if sum(x)>=.99*n %Defines situation where full diffusion occurs
+        %     flag=1; %Breaks the while loop
+        % end
+        y=y+abs(old-x); %adds 1 to any element of y whose agent has switched
+        old=x; %Updates t-1 (Useful for x_hat)
     end
-    Total_unequals=n-1-Total_equals;
-    a_base = b./(g*Total_equals + Total_unequals);
-    denominator=(g*Total_equals + Total_unequals);
-    a_all=a_base.*G;
-    P_1=x'.*a_all;
-    S_C_1=sum(P_1,2);
-    P_0=(1-x').*a_all;
-    S_C_0=sum(P_0,2);
-    pi(:,1)=S_C_0+k.*(1-x)+r.*(1-p); %SQ (0)
-    pi(:,2)=S_C_1+k.*x+r.*p; %Alt (1)
-    x=zeros(n,1);
-    x(rand(n,1)<exp(beta*pi(:,2))./(exp(beta*pi(:,2))+exp(beta*pi(:,1))))=1;
-    %If rand number [0,1] < prob(alt), then agent plays alt
-    x(s==1)=1; %Those who are CM will play strat 1 (alt)
-    p=.5*(1+(sum(x)-x)/(n-1)-(sum(old)-old)/(n-1)); %Updates x_hat
-    z=[z sum(x)];
-     if sum(x)<=.4*n
-         dt=t; %dt will stop at take-off time
+    dt=t-dt; %dt becomes a measure of explosiveness
+    y=y(1:round((1-n_s)*n))'-1; %Formula for switching rate (Not divided by diffusion time)
+    if t>400
+        z=reducev(z,0:t,200);
     end
-     if sum(x)>=.99*n %Defines situation where full diffusion occurs
-         flag=1; %Breaks the while loop
-     end
-    y=y+abs(old-x); %adds 1 to any element of y whose agent has switched
-    old=x; %Updates t-1 (Useful for x_hat)
-end
-dt=t-dt; %dt becomes a measure of explosiveness
-y=y(1:round((1-n_s)*n))'-1; %Formula for switching rate (Not divided by diffusion time)
-if t>400
-    z=reducev(z,0:t,200);
-end
-plot(linspace(0,t,length(z)),(z-n_s*n)/(n-n_s*n))
+    if rho == 0.6
+        if runs < 10
+            plot(linspace(0,t,length(z)),(z-n_s*n)/(n-n_s*n),'color','#cce0ff')
+        end
+        if runs == 10
+            plot(linspace(0,t,length(z)),(z-n_s*n)/(n-n_s*n),'color','#0000CC')
+        end
+    end
+    if rho == 0.2
+        if runs < 10
+            plot(linspace(0,t,length(z)),(z-n_s*n)/(n-n_s*n),'color','#ffd699')
+        end
+        if runs == 10
+            plot(linspace(0,t,length(z)),(z-n_s*n)/(n-n_s*n),'color','#ff9900')
+        end
+    end
+    end
 end
--- Paper_Code/Fig2/fig2.m
+++ Paper_Code/Fig2/fig2.m
@@ -7,13 +7,17 @@ r_f=0.16;
 k_e=0.10;
 k_f=0.42; 
 %b is defined in the SocDynK function
-rho=0.2; %Percentage of explorers
+rho=0.6; %Percentage of explorers
 n_e=round(n*rho*3/4); %No. explorers = No.participants*percentage explorers*percentage non-committed
-[t,dt,y]=SocDynK_time2(n,beta,[r_e*ones(1,n_e) r_f*ones(1,n-n_e)],[k_e*ones(1,n_e) k_f*ones(1,n-n_e)],n/4); %Plots line where rho=0.2
-rho=0.6;
+[t,dt,y]=SocDynK_time2(n,beta,[r_e*ones(1,n_e) r_f*ones(1,n-n_e)],[k_e*ones(1,n_e) k_f*ones(1,n-n_e)],n/4,rho); %Plots line where rho=0.2
+rho=0.2;
 hold on %Allows multiple lines for fig 2a
 n_e=round(n*rho*3/4);
-[t2,dt2,y2]=SocDynK_time2(n,beta,[r_e*ones(1,n_e) r_f*ones(1,n-n_e)],[k_e*ones(1,n_e) k_f*ones(1,n-n_e)],n/4);
+[t2,dt2,y2]=SocDynK_time2(n,beta,[r_e*ones(1,n_e) r_f*ones(1,n-n_e)],[k_e*ones(1,n_e) k_f*ones(1,n-n_e)],n/4,rho);
+title('Original Fig2a')
+xlabel('Round, t')
+ylabel('Players Adopting the Alternative')
+legend('\rho_{e} = 0.6','\rho_{e} = 0.2')
 figure %Used to create new figure window
 histogram(y/t,14,'Normalization','probability')
 figure
